a991332b8fd4d0d186faf3f1ee5f37c630c5beac
Balieiro004 <Balieiro004@gmail.com>
2025-06-04 20:16:10 -0300

    Adicionando Swagger no projeto e fazendo a configuração de segurança para liberar

--- src/main/java/com/gestao/gestaoequipamentos/config/SecurityConfigurations.java
+++ src/main/java/com/gestao/gestaoequipamentos/config/SecurityConfigurations.java
@@ -27,6 +27,7 @@ public class SecurityConfigurations {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/colaboradores").hasRole("ADMIN")
